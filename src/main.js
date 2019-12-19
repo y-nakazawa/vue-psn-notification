f4a460d88639466dd2341a31a420f911f9d866fc
中澤祐一 <y.nakazawa1220@gmail.com>
2019-12-19 22:09:30 +0900

    Amplify設定の読み込み

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import awsconfig from './aws-exports';
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
 
+Amplify.configure(awsconfig);
+Vue.use(AmplifyPlugin, AmplifyModules);
 Vue.config.productionTip = false
 
 new Vue({
